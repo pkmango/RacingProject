9fa3a927896910f32a6529f747794945afa721a1
pkmango <pk-man@yandex.ru>
2021-04-24 09:02:00 +0300

    TrainingCamera

---
+++ Assets/Scripts/TrainingCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingCamera : MonoBehaviour
+{
+    public GameObject car;
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(car.transform.position.x, transform.position.y, car.transform.position.z);
+    }
+}
