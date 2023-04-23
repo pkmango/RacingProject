e5e8dc1d592d454b9d348a2eb994bda3b659378b
pkmango <33579530+pkmango@users.noreply.github.com>
2023-04-24 00:02:09 +0300

    Choosing a car in the store

--- Assets/Scripts/UI/BuyingCar.cs
+++ Assets/Scripts/UI/BuyingCar.cs
@@ -1,26 +1,58 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class BuyingCar : MonoBehaviour
 {
     [SerializeField]
-    private GameObject[] carList;
+    private ShopCar[] carList; // Массив с доступными для покупки авто
     [SerializeField]
     private GameObject playerCar;
+    [SerializeField]
+    private Text carName;
+    [SerializeField]
+    private Text carPrice;
 
-    public Vector3 carPosition;
-    public Quaternion carRotation;
-
-    private GameObject currentCar;
+    private ShopCar currentCar;
+    private int nextIndex = 0; // Индекс следющего элемента массива авто после нажатия на стрелку
 
     private void OnEnable()
     {
-        playerCar.SetActive(false);
-        currentCar = Instantiate(carList[0], carPosition, carRotation);
+        ShowNextCar(playerCar);
+    }
+
+    public void LeftArrowCick()
+    {
+        if (--nextIndex < 0)
+            nextIndex = carList.Length - 1;
+
+        ShowNextCar(currentCar.gameObject);
+    }
+
+    public void RightArrowClick()
+    {
+        if (++nextIndex > carList.Length - 1)
+            nextIndex = 0;
+
+        ShowNextCar(currentCar.gameObject);
+    }
+
+    private void ShowNextCar(GameObject objectToTide)
+    {
+        objectToTide.SetActive(false); // Пряем текущий авто, при первом запуске это машина игрока
+        currentCar = carList[nextIndex];
+        currentCar.gameObject.SetActive(true);
+        carName.text = currentCar.carName;
+        carPrice.text = currentCar.carPrice.ToString("N0") + " $";
     }
 
     private void OnDisable()
     {
-        if(playerCar != null)
+        nextIndex = 0;
+        
+        if (currentCar != null)
+            currentCar.gameObject.SetActive(false);
+
+        if (playerCar != null)
             playerCar.SetActive(true);
     }
 }
