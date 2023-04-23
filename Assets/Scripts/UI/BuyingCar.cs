722cadb5f14d6f62f7920d739fc35367e088f3e5
pkmango <33579530+pkmango@users.noreply.github.com>
2023-04-23 10:10:57 +0300

    New Car

---
+++ Assets/Scripts/UI/BuyingCar.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class BuyingCar : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] carList;
+    [SerializeField]
+    private GameObject playerCar;
+
+    public Vector3 carPosition;
+    public Quaternion carRotation;
+
+    private GameObject currentCar;
+
+    private void OnEnable()
+    {
+        playerCar.SetActive(false);
+        currentCar = Instantiate(carList[0], carPosition, carRotation);
+    }
+
+    private void OnDisable()
+    {
+        if(playerCar != null)
+            playerCar.SetActive(true);
+    }
+}
