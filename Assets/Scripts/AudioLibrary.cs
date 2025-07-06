7d5a4dec6559556f7e0f1e7d246cade00b89118c
pkmango <33579530+pkmango@users.noreply.github.com>
2025-07-06 13:41:32 +0300

    Added AudioLibrary and some sfx

---
+++ Assets/Scripts/AudioLibrary.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "AudioLibrary", menuName = "Audio/Audio Library")]
+public class AudioLibrary : ScriptableObject
+{
+    [System.Serializable]
+    public class Sound
+    {
+        public string name;
+        public AudioClip clip;
+        [Range(0, 1)] public float volume = 1;
+        public bool loop;
+    }
+
+    public Sound[] SFX;
+    public Sound[] Music;
+}
---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [SerializeField] private AudioLibrary library;
+    private Dictionary<string, AudioClip> sfxClips = new Dictionary<string, AudioClip>();
+    private Dictionary<string, AudioClip> musicClips = new Dictionary<string, AudioClip>();
+    //[SerializeField] private AudioSource musicSource;
+    //[SerializeField] private AudioSource sfxSource;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        foreach (var sound in library.SFX)
+        {
+            sfxClips.Add(sound.name, sound.clip);
+        }
+
+        foreach (var sound in library.Music)
+        {
+            musicClips.Add(sound.name, sound.clip);
+        }
+    }
+
+    public void PlaySFX(string name, Vector3 point)
+    {
+        if (sfxClips.TryGetValue(name, out AudioClip clip))
+        {
+            AudioSource.PlayClipAtPoint(clip, point);
+        }
+    }
+
+    public void PlayMusic(string name, Vector3 point)
+    {
+        if (sfxClips.TryGetValue(name, out AudioClip clip))
+        {
+            AudioSource.PlayClipAtPoint(clip, point);
+        }
+    }
+
+    //public void PlayMusic(AudioClip clip, bool loop = true)
+    //{
+    //    musicSource.clip = clip;
+    //    musicSource.loop = loop;
+    //    musicSource.Play();
+    //}
+
+    //public void PlaySFX(AudioClip clip, float volume = 1)
+    //{
+    //    sfxSource.PlayOneShot(clip, volume);
+    //}
+}
