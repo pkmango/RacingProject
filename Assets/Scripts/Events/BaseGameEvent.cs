5132764d26ab8f5bcf4483b9300b3f099e65522e
pkmango <33579530+pkmango@users.noreply.github.com>
2025-08-13 01:29:43 +0300

    Added classes for SO-events

---
+++ Assets/Scripts/Events/BaseGameEvent.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+// Этот класс не будет создаваться как ассет, он просто база для других
+public abstract class BaseGameEvent : ScriptableObject
+{
+    // Оставляем поле для описания в инспекторе
+    [TextArea] public string description;
+}
---
+++ Assets/Scripts/Events/GameEvent.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine.Events;
+
+// Эти классы будут содержать всю логику для событий с параметрами, но мы не будем создавать их экземпляры напрямую
+public abstract class GameEvent<T0> : BaseGameEvent
+{
+    private event UnityAction<T0> OnRaised;
+
+    public void Raise(T0 value)
+    {
+        OnRaised?.Invoke(value);
+    }
+
+    public void AddListener(UnityAction<T0> listener)
+    {
+        OnRaised += listener;
+    }
+
+    public void RemoveListener(UnityAction<T0> listener)
+    {
+        OnRaised -= listener;
+    }
+}
+
+public abstract class GameEvent<T0, T1> : BaseGameEvent
+{
+    private event UnityAction<T0, T1> OnRaised;
+
+    public void Raise(T0 value0, T1 value1)
+    {
+        OnRaised?.Invoke(value0, value1);
+    }
+
+    public void AddListener(UnityAction<T0, T1> listener)
+    {
+        OnRaised += listener;
+    }
+
+    public void RemoveListener(UnityAction<T0, T1> listener)
+    {
+        OnRaised -= listener;
+    }
+}
+
+public abstract class GameEvent<T0, T1, T2> : BaseGameEvent
+{
+    private event UnityAction<T0, T1, T2> OnRaised;
+
+    public void Raise(T0 value0, T1 value1, T2 value2)
+    {
+        OnRaised?.Invoke(value0, value1, value2);
+    }
+
+    public void AddListener(UnityAction<T0, T1, T2> listener)
+    {
+        OnRaised += listener;
+    }
+
+    public void RemoveListener(UnityAction<T0, T1, T2> listener)
+    {
+        OnRaised -= listener;
+    }
+}
